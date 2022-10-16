cfb9befe370e950d9cebd063e0b4c8f87cbf6ac0
oumasamzz <samomofayalgmail.com>
2022-10-16 22:24:28 +0300

    header

---
+++ src/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Header.css' 
+
+function Header() {
+  return (
+  
+  
+    <div className = 'Header__container'>
+    <div className = 'Header__container__left'>
+       
+   
+ 
+
+    </div>
+    <div className = 'Header__container__right'>
+
+
+    </div>
+
+    </div>
+    
+  
+    
+    
+    
+    
+  )
+}
+
+export default Header
